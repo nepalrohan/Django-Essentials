2802de5afbac0384b39fa9294118b33607102094
nepalrohan <rohannepal098@gmail.com>
2024-09-19 08:24:11 -0700

    Add model and migrate them

--- base/views.py
+++ base/views.py
@@ -20,3 +20,7 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
